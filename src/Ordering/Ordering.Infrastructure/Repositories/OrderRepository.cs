f979adc15baa24c5fcdd4d6028bbefb82509d7c1
zhandaaaulet <zh.myrzatayev@gmail.com>
2021-02-25 22:13:48 +0600

    fixed bugs

---
+++ src/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUsername(string username)
+        {
+            var listOfOrders = await _dbContext.Orders.Where(i => i.UserName == username).ToListAsync();
+            return listOfOrders;
+        }
+    }
+}
